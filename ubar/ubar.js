f021a2bf5ca8c8f1eb305cae34ad00208b36ce4c
SaintEmbers <quantum108@gmail.com>
2015-06-08 12:04:58 -0700

    meteor oauth wip

--- ubar/ubar.js
+++ ubar/ubar.js
@@ -1,23 +1,41 @@
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.login.events({
+    'click #uber-login': function(event) {
+        Meteor.loginWithUber(function(err){
+            if (err) {
+                throw new Meteor.Error("Uber login failed");
+            }
+        });
+    },
+ 
+    'click #logout': function(event) {
+        Meteor.logout(function(err){
+            if (err) {
+                throw new Meteor.Error("Logout failed");
+            }
+        })
     }
   });
 }
+
 
 if (Meteor.isServer) {
+  
   Meteor.startup(function () {
+    ServiceConfiguration.configurations.remove({
+    service: 'uber'
+    }); 
+    ServiceConfiguration.configurations.insert({
+      service : "uber", 
+      client_id : "QUcKJJhnZIlkYM4_bnCkK5FbXbPrpELz", 
+      client_secret : "IQ5FBDEdScBuHgIreXuYUd6rbvWHYiRVnrRsObEQ", 
+      server_token : "GsbyGzKLvuFY_5HKnDRwQ1X-qI0ZovFd6UV21Ate", 
+      redirect_uri : "http://localhost:3000/auth/uber/callback" 
+    })
     // code to run on server at startup
+    console.log('server up!')
   });
 }
+
