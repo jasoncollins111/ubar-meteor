612d88e3fa2e4e8f219f3d7fcf44ec637d5b30c6
SaintEmbers <quantum108@gmail.com>
2015-07-12 17:06:41 -0700

    shell functions

--- ubar/server/uberServer.js
+++ ubar/server/uberServer.js
@@ -3,51 +3,24 @@ if (Meteor.isServer) {
 
     ServiceConfiguration.configurations.remove({
     service: 'uber'
-    }); 
-    
+    });
+
     ServiceConfiguration.configurations.insert({
-      service : "uber", 
-      client_id : "QUcKJJhnZIlkYM4_bnCkK5FbXbPrpELz", 
-      client_secret : "IQ5FBDEdScBuHgIreXuYUd6rbvWHYiRVnrRsObEQ", 
-      server_token : "GsbyGzKLvuFY_5HKnDRwQ1X-qI0ZovFd6UV21Ate",       
-      redirect_uri : "http://localhost:3000/_oauth/uber?close" 
+      service : "uber",
+      client_id : "YOUR UBER CLIENT ID",
+      client_secret : "YOUR UBER CLIENT SECRET",
+      server_token : "YOUR UBER SERVER TOKEN",
+      redirect_uri : "http://localhost:3000/_oauth/uber?close"
 
-
-
-      // client_id : Meteor.settings.client_id, 
-      // client_secret : Meteor.settings.client_secret, 
-      // server_token : Meteor.settings.server_token, 
-      // redirect_uri : Meteor.settings.redirect_uri 
-      // http://localhost:3000/_oauth/uber?close
-      // https://ubar.meteor.com/_oauth/uber?close
     })
   })
   Meteor.methods({
- 
+
     getAuthorizedRequest: function(endpoint, accessToken, callback) {
-      var params = {
-        headers: {
-          Authorization: "Bearer " + accessToken
-        }
-      }
-      var response = Meteor.http.call('GET', 'https://sandbox-api.uber.com'+endpoint, params).data
-      console.log(response)
-      return response
+     //FILL OUT GET REQUEST TO UBER API
     },
     postAuthorizedRequest: function(endpoint, accessToken, parameters, callback) {
-      this.unblock()
-      var params = {
-        data: parameters,
-
-        headers: {
-          Authorization: "Bearer " + accessToken,
-          'Content-Type': 'application/json'
-        }
-      }
-      var response = HTTP.post('https://sandbox-api.uber.com'+endpoint, params).data
-      console.log("response is:",response)
-      return response
-    }
+      //FILL OUT POST REQUEST TO UBER API
   });
   console.log('server up!')
 }
