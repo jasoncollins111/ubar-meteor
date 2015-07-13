37c6dbeb818a7f523e6ae32125e6421c70ce7e0f
SaintEmbers <quantum108@gmail.com>
2015-07-12 17:44:33 -0700

    working version

--- ubar/client/uberClient.js
+++ ubar/client/uberClient.js
@@ -1,5 +1,5 @@
 if (Meteor.isClient) {
-  
+
   Template.home.helpers({
     location: function() {
       var location = Geolocation.currentLocation()
@@ -8,7 +8,7 @@ if (Meteor.isClient) {
       Session.set('latitude', latitude)
       Session.set('longitude', longitude)
       GoogleMaps.load({key: 'AIzaSyCtNUFOJI2JKSkUr20zDjhst0Ko8xSruP8'});
-      // var local = 
+      // var local =
       console.log()
       return latitude +' '+longitude
     },
@@ -24,7 +24,7 @@ if (Meteor.isClient) {
     }
   })
 
-  Template.map.helpers({  
+  Template.map.helpers({
     mapOptions: function() {
       var lat = Session.get('latitude')
       var lon = Session.get('longitude')
@@ -33,11 +33,11 @@ if (Meteor.isClient) {
           center: new google.maps.LatLng(lat, lon),
           zoom: 17
         };
-      } 
+      }
     }
   });
 
-  Template.map.onCreated(function() {  
+  Template.map.onCreated(function() {
     GoogleMaps.ready('map', function(map) {
         var lat = Session.get('latitude')
         var lng = Session.get('longitude')
@@ -46,9 +46,9 @@ if (Meteor.isClient) {
           animation: google.maps.Animation.DROP,
           position: new google.maps.LatLng(lat, lng),
           map: map.instance
-        });   
-    }); 
-  }); 
+        });
+    });
+  });
   Template.home.events({
     'click #uber-login': function(event) {
         Meteor.loginWithUber({ loginStyle: 'redirect' }, function(err){
@@ -61,7 +61,7 @@ if (Meteor.isClient) {
             }
         });
     },
- 
+
     'click #logout': function(event) {
         Meteor.logout(function(err){
             if (err) {
@@ -71,19 +71,33 @@ if (Meteor.isClient) {
     },
     'click #request': function() {
       var user = Meteor.users.find().fetch();
-      var accessToken = user[0].services.uber.accessToken 
+      var accessToken = user[0].services.uber.accessToken
       var latitude = Session.get('latitude')
       var longitude = Session.get('longitude')
-      
+
+      var uberX;
+
+      Meteor.call('getAuthorizedRequest', '/v1/products?latitude='+latitude+'&longitude='+longitude, accessToken, function(err,res){
+        if(err){
+          throw new Meteor.Error('request failed at get',err)
+        } else{
+          console.log('get request:',res)
+          uberX = res.products[0].product_id
+        }
+      })
+
       var params = {
         start_latitude: latitude,
         start_longitude: longitude,
-        product_id: "a1111c8c-c720-46c3-8534-2fcdd730040d"
+        product_id: "2143f90b-ce68-4f6d-a113-4872b207e626"
       }
+
+
+
 
       Meteor.call('postAuthorizedRequest', '/v1/requests', accessToken, params,  function(err, res){
         if (err) {
-          throw new Meteor.Error('request failed here', err)
+          throw new Meteor.Error('post request failed here', err)
         } else{
           console.log("request results:",res)
           Session.set('eta', res.eta)
--- ubar/server/uberServer.js
+++ ubar/server/uberServer.js
@@ -3,27 +3,19 @@ if (Meteor.isServer) {
 
     ServiceConfiguration.configurations.remove({
     service: 'uber'
-    }); 
-    
+    });
+
     ServiceConfiguration.configurations.insert({
-      service : "uber", 
-      client_id : "QUcKJJhnZIlkYM4_bnCkK5FbXbPrpELz", 
-      client_secret : "IQ5FBDEdScBuHgIreXuYUd6rbvWHYiRVnrRsObEQ", 
-      server_token : "GsbyGzKLvuFY_5HKnDRwQ1X-qI0ZovFd6UV21Ate",       
-      redirect_uri : "http://localhost:3000/_oauth/uber?close" 
+      service : "uber",
+      client_id : "QUcKJJhnZIlkYM4_bnCkK5FbXbPrpELz",
+      client_secret : "IQ5FBDEdScBuHgIreXuYUd6rbvWHYiRVnrRsObEQ",
+      server_token : "GsbyGzKLvuFY_5HKnDRwQ1X-qI0ZovFd6UV21Ate",
+      redirect_uri : "http://localhost:3000/_oauth/uber?close"
 
-
-
-      // client_id : Meteor.settings.client_id, 
-      // client_secret : Meteor.settings.client_secret, 
-      // server_token : Meteor.settings.server_token, 
-      // redirect_uri : Meteor.settings.redirect_uri 
-      // http://localhost:3000/_oauth/uber?close
-      // https://ubar.meteor.com/_oauth/uber?close
     })
   })
   Meteor.methods({
- 
+
     getAuthorizedRequest: function(endpoint, accessToken, callback) {
       var params = {
         headers: {
